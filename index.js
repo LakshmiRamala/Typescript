b2ec1d93045c307815114908b0a12d9134129d39
LakshmiRamala <lakshmiramala1425@gmail.com>
2024-11-17 20:12:32 +0530

    Abstart classes

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Department = /** @class */ (function () {
-    function Department(name) {
+"use strict";
+class Department {
+    constructor(name) {
         this.name = name;
     }
-    return Department;
-}());
-var ItDepartment = /** @class */ (function (_super) {
-    __extends(ItDepartment, _super);
-    function ItDepartment() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.holidays = [];
-        return _this;
+    addHolidays(holidays) {
+        if (Array.isArray(holidays)) {
+            for (const holiday of holidays) {
+                this.holidays.push(holiday);
+            }
+        }
     }
-    return ItDepartment;
-}(Department));
-var AdminDepartment = /** @class */ (function (_super) {
-    __extends(AdminDepartment, _super);
-    function AdminDepartment() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.holidays = [];
-        return _this;
+}
+class ItDepartment extends Department {
+    constructor() {
+        super(...arguments);
+        this.holidays = [];
     }
-    return AdminDepartment;
-}(Department));
-console.log("hi");
+}
+class AdminDepartment extends Department {
+    constructor() {
+        super(...arguments);
+        this.holidays = [];
+    }
+}
--- index.ts
+++ index.ts
@@ -1,18 +1,25 @@
 type Holidays = {
-  date: Date;
-  reason: string;
+    date: Date;
+    reason: string;
 }[];
 
 abstract class Department {
-  protected abstract holidays: Holidays;
-  protected constructor(protected name: string) {}
+    protected abstract holidays: Holidays;
+    protected constructor(protected name: string) {}
+
+    public addHolidays(holidays: Holidays){
+        if(Array.isArray(holidays)){
+            for (const holiday of holidays){
+                this.holidays.push(holiday)
+            }
+        }
+    }
 }
 
 class ItDepartment extends Department {
-  protected holidays: Holidays = [];
+    protected holidays: Holidays = [];
 }
 
 class AdminDepartment extends Department {
-  protected holidays: Holidays = [];
+    protected holidays: Holidays = [];
 }
-console.log("hi");
