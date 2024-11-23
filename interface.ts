845c71863a458c88e57c4a8cb2300fc16cc2a8c3
LakshmiRamala <lakshmiramala1425@gmail.com>
2024-11-23 11:41:15 +0530

    [23 nov 24 interface]

---
+++ interface.ts
@@ -0,0 +1,12 @@
+interface Person{
+  name:string,
+  age:number
+  email:string,
+  phoneNumber:number,
+}
+const  person:Person={
+   name:"Lakshmi",
+   age:22,
+   email:"naga@1425",
+   phoneNumber:9014992165
+};
