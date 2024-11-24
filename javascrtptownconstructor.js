75a3624483e1a7a788b3cc4b1c6ef1c66bc96c64
LakshmiRamala <lakshmiramala1425@gmail.com>
2024-11-24 18:58:14 +0530

    [24 nov 24] Javascript own constructor function

---
+++ javascrtptownconstructor.js
@@ -0,0 +1,49 @@
+/*
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+    this.points = 0;
+  }
+
+  login() {
+    console.log(this.name, 'Has logged in');
+  }
+
+  logout() {
+    console.log(this.name, 'Has logged out');
+  }
+
+  addPoint() {
+    this.points++;
+    console.log('total points', this.points);
+  }
+}
+
+const user = new User('John', 'john@email.com');
+
+console.log(user);
+*/
+
+function User(name, email) {
+    this.name = name;
+    this.email = email;
+    this.points = 0;
+  
+    this.login = () => {
+      console.log(this.name, 'Has logged in');
+    };
+  
+    this.logout = () => {
+      console.log(this.name, 'Has logged out');
+    };
+  
+    this.addPoint = () => {
+      this.points++;
+      console.log('total points', this.points);
+    };
+  }
+  
+  const user = new User('John', 'john@email.com');
+  
+  console.log(user);
